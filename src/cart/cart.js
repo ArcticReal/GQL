1015dff4c5680ba9d091d331fc1968cc8b4adb0a
Alexander T. Bratrich <alexander@bratrich-online.de>
2017-11-30 10:35:21 +0100

    added stuff to cart/order (still not working)

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -1,19 +1,18 @@
 import { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLString, GraphQLInt, GraphQLBoolean,
 } from 'graphql';
+import { ProductType } from '../product/product.js';
 
 const CartType = new GraphQLObjectType({
   name: 'Cart',
   description: '...',
 
   fields: () => ({
-    cartId: { type: GraphQLString }
+    cartId: { type: GraphQLString },
+    total: {type: GraphQLString},
+    items: {type: new GraphQLList(ProductType) },
+    numberItems: {type: new GraphQLList(GraphQLInt) }
   })
 });
-
-/*regions: {
-      type: new GraphQLList(CartType),
-      resolve: (geo, args, {loaders}) => loaders.ofbiz.load(`cart/show`)
-    }*/
 
 //resolve: (geo, args, {loaders}) => loaders.ofbiz.load(`cart/show`)
 
--- src/order/order.js
+++ src/order/order.js
@@ -1,12 +1,18 @@
 import { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLString, GraphQLInt, GraphQLBoolean,
 } from 'graphql';
+import { ProductType } from '../product/product.js';
 
 const OrderType = new GraphQLObjectType({
   name: 'Order',
   description: '...',
 
   fields: () => ({
-    orderId: { type: GraphQLString }
+    orderId: { type: GraphQLString },
+    statusId: { type: GraphQLString },
+    grandTotal: { type: GraphQLString },
+    orderDate: { type: GraphQLString },
+    items: { type: new GraphQLList(ProductType) },
+    numberItems: {type: new GraphQLList(GraphQLInt) }
   })
 });
 
